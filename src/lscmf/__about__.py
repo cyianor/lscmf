d6a20de799607dff2662634017313188d0b32576
Felix Held <felix.held@gmail.com>
2024-12-25 00:42:58 +0100

    adjusted version to appease PyPi

--- src/lscmf/__about__.py
+++ src/lscmf/__about__.py
@@ -1,6 +1,6 @@
 __title__ = "lscmf"
 __description__ = "Large-scale Collective Matrix Factorization"
-__version__ = "0.2.1"
+__version__ = "0.2.1-1"
 __author__ = "Felix Held"
 __author_email__ = "felix.held@gmail.com"
 __license__ = "bsd-3-clause"
